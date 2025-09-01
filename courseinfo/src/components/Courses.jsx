b9160a0cc9bd1e9e25b4bd4ebf0c6eb8f8256ea7
Roberto Caravaca <robertocaravaca436@gmail.com>
2025-09-01 11:53:08 +0200

    courseinfo - step 9

---
+++ courseinfo/src/components/Courses.jsx
@@ -0,0 +1,14 @@
+import Course from "./Course";
+
+const Courses = ({courses}) => {
+
+  console.log('Courses', courses);
+  
+  return (
+    <div>
+      {courses.map(course => <Course key={course.id} course={course}/>)}
+    </div>
+  )
+}
+
+export default Courses
